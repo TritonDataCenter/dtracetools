95f9287332598fd3a5a6306e5753e3c20f41209f
Derek Crudgington <derek@joyent.com>
2014-07-07 16:04:32 +0000

    Added dtrace-apache* scripts to go with mod_usdt

---
+++ dtrace-apache_requests_bylatency.d
@@ -0,0 +1,19 @@
+#!/usr/sbin/dtrace -s
+
+#pragma D option quiet
+
+BEGIN
+{
+        printf("Tracing.  Hit CTRL-C to stop.\n");
+}
+
+httpd*:::request-start
+{
+        self->start = timestamp;
+}
+
+httpd*:::request-done
+{
+        @["microseconds"] = quantize((timestamp - self->start) / 1000);
+        self->start = 0;
+}
